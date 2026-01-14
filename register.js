f99b529cd55116dfb0e01c2102e1969e02bcef54
Engrkartik <engr.kartik2014@gmail.com>
2026-01-14 18:14:59 +0530

    update file by login

--- register.js
+++ register.js
@@ -1,4 +1,2 @@
 console.log('userregistatuion')
-console.log('userregistatuion from user-registration')
-
-console.log('userregistion check by login')
+console.log('userregistatuion from user-regiseetration')
